0bb42d6752147358bc60e5bff453c37e2f09846f
FishaPL <37590200+FishaPL@users.noreply.github.com>
2018-04-09 19:08:12 +0200

    Nie mam pojęcia co robie
    First Commit ?

---
+++ Office Management System/src/sample/SqliteConnection.java
@@ -0,0 +1,22 @@
+package sample;
+
+import java.sql.*;
+
+public class SqliteConnection
+{
+    public static Connection Connector()
+    {
+        try
+        {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jfbc:sqlite:EmployeeDb.sqlite");
+            // albo bez ścieżki i wrzucić baze do projektu
+            return conn;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
