ef9ff6999d78a2601806fdebc8f271a071a9d7f4
damian2127 <famar2@o2.pl>
2018-05-06 23:34:19 +0200

    add AdminController

---
+++ src/main/java/com/example/controller/AdminController.java
@@ -0,0 +1,51 @@
+package com.example.controller;
+
+import com.example.model.User;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+@Controller
+public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value="/admin/add_employee", method = RequestMethod.GET)
+    public ModelAndView add_employee(){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("/admin/add_employee");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/admin/add_employee", method = RequestMethod.POST)
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userService.findUserByEmail(user.getEmail());
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user",
+                            "There is already a user registered with the email provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("admin/add_employee");
+        } else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("admin/add_employee");
+
+        }
+        return modelAndView;
+    }
+}
